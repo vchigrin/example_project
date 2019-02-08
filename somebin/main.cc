95f5ece959c728bc661a2ae81e37c0fa99e4bbd4
Vyacheslav Chigrin <vchigrin@yandex-team.ru>
2019-02-08 16:01:45 +0300

    Demo bazel project

---
+++ somebin/main.cc
@@ -0,0 +1,6 @@
+#include "somelib/test.h"
+
+int main(int argc, char* argv[]) {
+  test();
+  return 0;
+}
---
+++ somelib/test.cc
@@ -0,0 +1,7 @@
+#include "test.h"
+
+#include <iostream>
+
+void test() {
+  std::cout << "Hello, World" << std::endl;
+}
